359fe452caeec3decd61c700bb437fc6c6caee45
bkc-tomi <bkc.tomi1979@gmail.com>
2022-01-10 11:53:04 +0900

    q012~014を追加&q011修正

--- src/section_2/q011.rs
+++ src/section_2/q011.rs
@@ -1,6 +1,6 @@
 /**
  * 正の整数nが与えられて、n以下の素数とその個数を返すプログラム
- * 制約: (1 <= n <= 65535)
+ * 制約: (3 <= n <= 65535)
  */
 pub fn show_prime_number(n: u16) -> (Vec<u16>, u16) {
     let mut prime_arr:Vec<u16> = vec![2];
